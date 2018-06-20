699278212c93ddbfdd19364ea0875eb8ace29d8b
Pavel Levchuk <SpaierInstinct@gmail.com>
2018-06-20 04:17:12 +0300

    src: add model error codes

---
+++ src/Spaier.Recaptcha/ModelErrorCodes.cs
@@ -0,0 +1,8 @@
+﻿namespace Spaier.Recaptcha
+{
+    public static class ModelErrorCodes
+    {
+        public const string NoRecaptchaResponseError = "no-recaptcha-response";
+        public const string NoRecaptchaResponseMessage = "reCAPTCHA is missing";
+    }
+}
