bdfa203929a789eec9007bd20cd704747825482e
Pavel Levchuk <SpaierInstinct@gmail.com>
2018-06-20 04:17:50 +0300

    src: add recaptcha error codes

---
+++ src/Spaier.Recaptcha/RecaptchaErrorCodes.cs
@@ -0,0 +1,30 @@
+﻿namespace Spaier.Recaptcha
+{
+    public static class RecaptchaErrorCodes
+    {
+        /// <summary>
+        /// The secret parameter is missing.
+        /// </summary>
+        public const string MissingInputSecret = "missing-input-secret";
+
+        /// <summary>
+        /// The secret parameter is invalid or malformed.
+        /// </summary>
+        public const string InvalidInputSecret = "invalid-input-secret";
+
+        /// <summary>
+        /// The response parameter is missing.
+        /// </summary>
+        public const string MissingInputResponse = "missing-input-response";
+
+        /// <summary>
+        /// The response parameter is invalid or malformed.
+        /// </summary>
+        public const string InvalidInputResponse = "invalid-input-response";
+
+        /// <summary>
+        /// The request is invalid or malformed.
+        /// </summary>
+        public const string BadRequest = "bad-request";
+    }
+}
