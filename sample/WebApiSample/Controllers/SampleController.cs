6d460c8659e9bcf4faa2529b14ed4e2fd9e5582d
Pavel Levchuk <SpaierInstinct@gmail.com>
2018-08-02 22:39:34 +0300

    src(sample): add sample

---
+++ sample/WebApiSample/Controllers/SampleController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Spaier.Recaptcha;
+using Spaier.Recaptcha.Http;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApiSample.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class SampleController : Controller
+    {
+        private readonly IRecaptchaConfigurationProvider configurationProvider;
+        private readonly IRecaptchaTokenProvider tokenProvider;
+        private readonly IRecaptchaHttpClient httpClient;
+
+        public SampleController(IRecaptchaConfigurationProvider configurationProvider,
+            IRecaptchaTokenProvider tokenProvider, IRecaptchaHttpClient httpClient)
+        {
+            this.configurationProvider = configurationProvider ?? throw new ArgumentNullException(nameof(configurationProvider));
+            this.tokenProvider = tokenProvider ?? throw new ArgumentNullException(nameof(tokenProvider));
+            this.httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
+        }
+
+        // Manual.
+        public void Api1()
+        {
+            httpClient.VerifyRecaptchaAsync(configurationProvider.GetRecaptchaConfiguration(Request, out _), Request, tokenProvider);
+        }
+
+        // Any specified configuration
+        [ValidateRecaptcha]
+        public void Api2()
+        {
+        }
+
+        // Only V3 reCAPTCHA specified in Startup.cs is allowed
+        [ValidateRecaptcha(Configurations = new[] { "V3" })]
+        public void Api3()
+        {
+        }
+    }
+}
---
+++ sample/WebApiSample/Startup.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Spaier.Recaptcha;
+
+namespace WebApiSample
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddRecaptcha()
+                .AddTokenHeaderProvider()
+                .AddConfigurationHeaderProvider(options =>
+                {
+                    options.Configurations = new Dictionary<string, RecaptchaConfiguration>
+                    {
+                        [""] = new RecaptchaConfiguration(RecaptchaDefaults.TestSecretKey, RecaptchaSecretType.V2),
+                        ["Android"] = new RecaptchaConfiguration(RecaptchaDefaults.TestSecretKey, RecaptchaSecretType.V2Android),
+                        ["V3"] = new RecaptchaConfiguration(RecaptchaDefaults.TestSecretKey, RecaptchaSecretType.V3)
+                    };
+                })
+                .AddRecaptchaHttpClient()
+                .UseGoogleUrl();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseDeveloperExceptionPage();
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
