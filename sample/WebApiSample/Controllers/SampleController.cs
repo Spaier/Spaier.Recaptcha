1e918ac17d1310d4771fd794253aa8cba692b074
Pavel Levchuk <SpaierInstinct@gmail.com>
2018-08-06 21:39:41 +0300

    feat(sample): update sample

--- sample/WebApiSample/Controllers/SampleController.cs
+++ sample/WebApiSample/Controllers/SampleController.cs
@@ -1,47 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Spaier.Recaptcha;
-using Spaier.Recaptcha.Http;
-
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+using Spaier.Recaptcha.Responses;
 
 namespace WebApiSample.Controllers
 {
     [Route("api/[controller]/[action]")]
+    [Produces("application/json")]
     [ApiController]
-    public class SampleController : Controller
+    public class SampleController : ControllerBase
     {
-        private readonly IRecaptchaConfigurationProvider configurationProvider;
-        private readonly IRecaptchaTokenProvider tokenProvider;
-        private readonly IRecaptchaHttpClient httpClient;
-
-        public SampleController(IRecaptchaConfigurationProvider configurationProvider,
-            IRecaptchaTokenProvider tokenProvider, IRecaptchaHttpClient httpClient)
-        {
-            this.configurationProvider = configurationProvider ?? throw new ArgumentNullException(nameof(configurationProvider));
-            this.tokenProvider = tokenProvider ?? throw new ArgumentNullException(nameof(tokenProvider));
-            this.httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
-        }
-
-        // Manual.
-        public void Api1()
-        {
-            httpClient.VerifyRecaptchaAsync(configurationProvider.GetRecaptchaConfiguration(Request, out _), Request, tokenProvider);
-        }
-
         // Any specified configuration
         [ValidateRecaptcha]
-        public void Api2()
+        public Task<ActionResult> Api1()
         {
+            return Task.FromResult((ActionResult)NoContent());
         }
 
         // Only V3 reCAPTCHA specified in Startup.cs is allowed
-        [ValidateRecaptcha(Configurations = new[] { "V3" })]
-        public void Api3()
+        [ValidateRecaptcha(Configurations = new[] { "V3" }, MinimumScore = 0, AllowedAction = "api2")]
+        public Task<ActionResult> Api2([FromRecaptchaResponse] RecaptchaResponseV3 recaptchaResponse)
         {
+            Console.WriteLine(recaptchaResponse);
+            return Task.FromResult((ActionResult)NoContent());
         }
     }
 }
