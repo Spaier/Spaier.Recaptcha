1acde0e5e7f062d954496dc47061f27d7847b856
Pavel Levchuk <SpaierInstinct@gmail.com>
2018-08-06 19:57:05 +0300

    feat: add in memory configuration store

---
+++ src/Spaier.Recaptcha/DependencyInjection/BuilderExtensions/InMemory.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+using Spaier.Recaptcha;
+using Spaier.Recaptcha.DependencyInjection;
+using Spaier.Recaptcha.Stores.InMemory;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class InMemoryRecaptchaBuilderExtensions
+    {
+        public static IRecaptchaBuilder AddInMemoryConfigurationStore(this IRecaptchaBuilder builder,
+            IDictionary<string, RecaptchaConfiguration> configurations)
+        {
+            builder.Services.AddSingleton<IRecaptchaConfigurationStore, InMemoryRecaptchaConfigurationStore>(services
+                => new InMemoryRecaptchaConfigurationStore(configurations));
+            return builder;
+        }
+
+        public static IRecaptchaBuilder AddInMemoryConfigurationStore(this IRecaptchaBuilder builder,
+            IConfiguration configuration)
+        {
+            var configurations = configuration.Get<Dictionary<string, RecaptchaConfiguration>>();
+            return builder.AddInMemoryConfigurationStore(configurations);
+        }
+    }
+}
---
+++ src/Spaier.Recaptcha/Stores/InMemory/InMemoryRecaptchaConfigurationStore.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spaier.Recaptcha.Stores.InMemory
+{
+    public class InMemoryRecaptchaConfigurationStore : IRecaptchaConfigurationStore
+    {
+        private readonly IDictionary<string, RecaptchaConfiguration> configurations;
+
+        public InMemoryRecaptchaConfigurationStore(IDictionary<string, RecaptchaConfiguration> configurations)
+        {
+            this.configurations = configurations;
+        }
+
+        public Task<RecaptchaConfiguration> GetRecaptchaConfiguration(string key)
+        {
+            return Task.FromResult(configurations[key]);
+        }
+
+        public Task<IEnumerable<RecaptchaConfiguration>> GetRecaptchaConfigurations()
+        {
+            return Task.FromResult(configurations.Values.AsEnumerable());
+        }
+    }
+}
